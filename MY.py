d38133ab9ce3e33500b627a7bd0a7fa2f602a11d
overduz <s.a.seyedzadeh@gmail.com>
2025-04-16 13:45:42 +0330

    Add MY.py file

---
+++ MY.py
@@ -0,0 +1,8 @@
+class person:
+    def __init__(self,name,family,age):
+        self.name = name
+        self.family = family
+        self.age = age
+amirrak = person("amir","seyedzadeh", 35)        
+# print(amirrak)
+print(amirrak.family)
